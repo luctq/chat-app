68c774cebc0963428845c1d2e5ca33fedd34fbc5
luctq <tranquocluc610@gmail.com>
2022-12-13 23:28:15 +0700

    init project & register function

---
+++ client/src/utils/APIRoutes.js
@@ -0,0 +1,3 @@
+const host = 'http://localhost:4001';
+
+export const RegisterAPI = `${host}/api/auth/register`;
---
+++ server/index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const userRoutes = require('./routes/userRoutes');
+const app = express();
+require('dotenv').config();
+app.use(cors());
+app.use(express.json());
+
+app.use('/api/auth', userRoutes);
+mongoose.set('strictQuery', true);
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log('DB connection successfull');
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
+
+const server = app.listen(process.env.PORT, () => {
+  console.log(`Server started on Port ${process.env.PORT}`);
+});
